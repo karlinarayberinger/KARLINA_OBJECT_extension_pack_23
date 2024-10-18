86d6e89beb6218b2a1f768de0ac9c6a79134f689
karbytes <karlina.ray.beringer@protonmail.com>
2024-10-18 00:23:27 -0700

    Add files via upload

--- trigonometric_functions.cpp
+++ trigonometric_functions.cpp
@@ -1,7 +1,7 @@
 /**
  * file: trigonometric_functions.cpp
  * type: C++ (source file)
- * date: 17_OCTOBER_2024
+ * date: 18_OCTOBER_2024
  * author: karbytes
  * license: PUBLIC_DOMAIN 
  */
@@ -9,10 +9,12 @@
 /** preprocessing directives */
 #include <iostream> // standard input (std::cin), standard output (std::cout)
 #include <fstream> // file input, file output
-#define MAXIMUM_t 10000 // constant which represents maximum number of terms in each Taylor series
+#define MAXIMUM_i 10000 // constant which represents maximum number of iterations in Leibniz series
+#define MAXIMUM_t 10000 // constant which represents maximum number of terms in Taylor series
 #define MAXIMUM_x 10000 // constant which represents maximum value of x
 
 /** function prototypes */
+double computePi(int iterations);
 double sine(double x);
 double cosine(double x);
 double tangent(double x);
@@ -75,23 +77,110 @@ int main()
     return 0;
 }
 
-// Function to compute sine using the Taylor series
+/**
+ *---------------------------------------------------------------------------------------
+ * 
+ * This function computes the approximate value of Pi using the Leibniz series.
+ * 
+ * Pi ≈ 4 * (1 - (1 / 3) + (1 / 5) - (1 / 7) + (1 / 9) - ...)
+ * 
+ *---------------------------------------------------------------------------------------
+ * 
+ * Pi is a mathematical constant that is the ratio of a circle's circumference to 
+ * its diameter (which is approximately equal to 3.14159).
+ * 
+ * For more information on how to compute the approximate value of Pi
+ * (using a Monte Carlo dart-throwing simulation in JavaScript), visit
+ * the tutorial web page at the following Uniform Resource Locator:
+ * 
+ * https://karlinaobject.wordpress.com/pi_approximation/
+ * 
+ *---------------------------------------------------------------------------------------
+ * 
+ * iterations is assumed to be a nonnegative integer no larger than MAXIMUM_iteratons.
+ * 
+ *---------------------------------------------------------------------------------------
+ */
+double computePi(int iterations) 
+{
+    double pi = 0.0;
+    double sign = 1.0;  // alternates between positive and negative
+
+    if ((iterations < 0) || (iterations > MAXIMUM_i)) 
+    {
+        iterations = 1;
+        std::cout << "\n\nThe number of iterations for the Leibniz series in computePi(int iterations) was out of range. Hence it has been reset to 1.";
+    }
+
+    for (int i = 0; i < iterations; ++i) 
+    {
+        pi += sign / (2.0 * i + 1.0);  // add next term in the series
+        sign = -sign;  // alternate the sign for each term
+    }
+
+    pi *= 4.0;  // multiply by 4 to get Pi
+    return pi;
+}
+
+/**
+ *-----------------------------------------------------------------------------------------------------------------------------------
+ * 
+ * This function uses the Taylor series to compute the approximate value of sine of x.
+ * 
+ *-----------------------------------------------------------------------------------------------------------------------------------
+ * 
+ * The sine of an angle in a right triangle is defined as the ratio of the length of 
+ * the side opposite the angle to the length of the hypotenuse (the longest side of the triangle):
+ * 
+ * sin(x) = opposite / hypotenuse
+ * 
+ *-----------------------------------------------------------------------------------------------------------------------------------
+ * 
+ * Note that an angle measurement in degrees can be converted to radians using the following formula:
+ * 
+ * radians = degrees * (Pi / 180)
+ * 
+ * If x is 30 degrees, then the right triangle it is the interior angle measurement of is a triangle
+ * whose side opposite of x is 1 and whose hypotenuse is 2.
+ * 
+ * Hence, sine of 30 degrees can be computed as follows:
+ * 
+ * 30 degrees = Pi / 6 radians ≈ 0.5236
+ * 
+ * sin(0.5236) = 1 / 2 = 0.5
+ *
+ *-----------------------------------------------------------------------------------------------------------------------------------
+ * 
+ * x is an angle measurement in radians and can theoretically be any real number (but is assumed to be in [-MAXIMUM_x, MAXIMUM_x]).
+ * 
+ *-----------------------------------------------------------------------------------------------------------------------------------
+ */
 double sine(double x) 
 {
-    const int terms = MAXIMUM_t; // Number of terms in the Taylor series
+    int i = 0;
+    const int terms = MAXIMUM_t; // number of terms in the Taylor series
     double result = 0.0;
-    double term = x; // First term (x^1 / 1!)
-    int sign = 1; // Alternating signs for each term
-    for (int i = 1; i <= terms; ++i) 
+    double term = x; // first term: ((x ^ 1) / 1!)
+    int sign = 1; // alternating signs for each term
+    for (i = 1; i <= terms; i += 1) 
     {
         result += sign * term;
-        sign *= -1; // Alternating sign
-        term *= x * x / (2 * i * (2 * i + 1)); // Update term for the next iteration
+        sign *= -1; // alternating sign
+        term *= x * x / (2 * i * (2 * i + 1)); // update term for the next iteration
     }
     return result;
 }
 
-// Function to compute cosine using the Taylor series
+/**
+ * This function uses the Taylor series to compute the approximate value of cosine of x.
+ * 
+ * The cosine of an angle in a right triangle is defined as the ratio of the length of 
+ * the side adjacent to the angle to the length of the hypotenuse (the longest side of the triangle).
+ * 
+ * cos(x) = adjacent / hypotenuse
+ * 
+ * x is an angle measurement in radians and can theoretically be any real number (but is assumed to in [-MAXIMUM_x, MAXIMUM_x]).
+ */
 double cosine(double x) 
 {
     const int terms = MAXIMUM_t; // Number of terms in the Taylor series
@@ -107,7 +196,8 @@ double cosine(double x)
     return result;
 }
 
-// Tangent is sine / cosine
+/**
+ * This function computes the approximate value of tangent of x.
 double tangent(double x) 
 {
     return sine(x) / cosine(x);
